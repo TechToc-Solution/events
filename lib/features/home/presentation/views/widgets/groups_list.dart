19bc9492f9076db2b2311a74dc40c7efca94a150
AHNXD <nzamlyalhady@gmail.com>
2025-07-09 18:18:02 +0400

    optimize code

---
+++ lib/features/home/presentation/views/widgets/groups_list.dart
@@ -0,0 +1,47 @@
+import 'package:events/core/utils/app_localizations.dart';
+import 'package:events/core/utils/colors.dart';
+import 'package:events/features/home/data/model/home_model.dart';
+import 'package:flutter/material.dart';
+
+class GroupsList extends StatelessWidget {
+  const GroupsList({super.key, required this.data});
+
+  final HomeData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: data.groups?.length ?? 0,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: 4),
+          padding: EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: AppColors.primaryColors, width: 2),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: ListTile(
+            title: Text(
+              data.groups?[index].name ?? "",
+              style: TextStyle(
+                fontSize: 26,
+                color: AppColors.primaryColors,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              "${"group_hint_1".tr(context)} ${data.groups?[index].countSelect ?? 0} ${"group_hint_2".tr(context)}",
+            ),
+            trailing: CircleAvatar(
+              radius: 28,
+              backgroundColor: AppColors.primaryColors,
+              child: Icon(Icons.person, size: 32, color: Colors.white),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_page_body.dart
+++ lib/features/home/presentation/views/widgets/home_page_body.dart
@@ -2,6 +2,7 @@ import 'package:events/core/utils/app_localizations.dart';
 import 'package:events/core/utils/colors.dart';
 import 'package:events/features/home/data/model/home_model.dart';
 import 'package:events/features/home/presentation/views/widgets/countdown_timer_widget.dart';
+import 'package:events/features/home/presentation/views/widgets/groups_list.dart';
 import 'package:events/features/home/presentation/views/widgets/home_title.dart';
 import 'package:flutter/material.dart';
 
@@ -20,46 +21,7 @@ class HomePageBody extends StatelessWidget {
               SizedBox(height: 8),
               CountdownTimerWidget(),
               SizedBox(height: 8),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: data.groups?.length ?? 0,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(vertical: 4),
-                    padding: EdgeInsets.all(4),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(
-                        color: AppColors.primaryColors,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: ListTile(
-                      title: Text(
-                        data.groups?[index].name ?? "",
-                        style: TextStyle(
-                          fontSize: 26,
-                          color: AppColors.primaryColors,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(
-                        "${"group_hint_1".tr(context)} ${data.groups?[index].countSelect ?? 0} ${"group_hint_2".tr(context)}",
-                      ),
-                      trailing: CircleAvatar(
-                        radius: 28,
-                        backgroundColor: AppColors.primaryColors,
-                        child: Icon(
-                          Icons.person,
-                          size: 32,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
+              GroupsList(data: data),
             ],
           ),
         ),
